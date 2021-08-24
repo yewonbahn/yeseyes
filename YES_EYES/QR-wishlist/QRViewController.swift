42b5d3dcb4a978d036070f61469bfdeb03386951
yewonbahn <banyaewon98@gmail.com>
2021-08-24 17:15:43 +0900

    error fix

--- YES_EYES/QR-wishlist/QRViewController.swift
+++ YES_EYES/QR-wishlist/QRViewController.swift
@@ -5,6 +5,8 @@
 //  Created by mgpark on 2021/07/25
 
 import UIKit
+
+
 class QRCell: UITableViewCell{
     
     @IBOutlet weak var wishlistTitle: UILabel!
@@ -21,6 +23,7 @@ struct QRModel{
 }
 
 class QRViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, CartItemDelegate {
+
     func updateCartItem(cell: CartListTableViewCell, quantity: Int) {
         guard let indexPath = QRTableView.indexPath(for: cell) else { return }
         guard let cartItem = cart?.items[indexPath.row] else { return }
@@ -32,7 +35,6 @@ class QRViewController: UIViewController, UITableViewDelegate, UITableViewDataSo
     
     var cart: Cart? = nil
 
-    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 1
     }
@@ -40,22 +42,25 @@ class QRViewController: UIViewController, UITableViewDelegate, UITableViewDataSo
     func numberOfSections(in tableView: UITableView) -> Int {
         return cart?.items.count ?? 0
     }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    public var qrstr : String = "."
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) ->
+        UITableViewCell {
+        
+
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? CartListTableViewCell else { fatalError() }
         
         if let cartItem = cart?.items[indexPath.section]{
             
             cell.delegate = self as CartItemDelegate
-            print(cartItem.item)
+     
             cell.itemTitle.text = cartItem.item.title
             cell.itemPrice.text = cartItem.item.price
             
             cell.countLabel.text = String(describing: cartItem.quantity)
             cell.quantity = cartItem.quantity
+            qrstr.append(cartItem.item.title+cartItem.item.price+"\n")
         }
-        return cell
-    }
+        return cell}
     
     @IBOutlet weak var QrView: UIImageView!
     @IBOutlet weak var QRTableView: UITableView!
@@ -65,16 +70,15 @@ class QRViewController: UIViewController, UITableViewDelegate, UITableViewDataSo
         QRTableView.dataSource = self
         self.title = "QR·위시리스트"
         self.navigationController?.navigationBar.prefersLargeTitles = false
-     
-//        model.append(
-//            [QRModel(wishlist: "상품이 담겼습니다.")])
-//        // "Hello,world!" 가 Qr 로 형성되어있음
-        //
-        let QRCodeImage = generateQRCode(from: "Hello, world!")
+        self.QRTableView.reloadData()
+        // "Hello,world!" 가 Qr 로 형성되어있음
+        
+        let QRCodeImage = generateQRCode(from:qrstr)
         self.QrView.image = QRCodeImage
+        
+   
     }
     
- 
     func generateQRCode(from string: String) -> UIImage? {
         let data = string.data(using: String.Encoding.ascii)
       
@@ -90,4 +94,5 @@ class QRViewController: UIViewController, UITableViewDelegate, UITableViewDataSo
       
         return nil
     }
+ 
 }
