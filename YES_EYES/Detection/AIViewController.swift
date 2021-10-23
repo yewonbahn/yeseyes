dd28f0eb9976f4f66ffc11165cf0e84eb2ea22b2
sumin <sumin816@ewhain.net>
2021-10-23 17:20:37 +0900

    change model to hangul model

--- YES_EYES/Detection/AIViewController.swift
+++ YES_EYES/Detection/AIViewController.swift
@@ -9,7 +9,7 @@ class AIViewController: UIViewController {
     
     let videoCapture : VideoCapture = VideoCapture()
     let context = CIContext()
-    let model: MyImageClassifier = try! MyImageClassifier(configuration: .init())
+    let model: hangulmodel = try! hangulmodel(configuration: .init())
     
     override func viewDidLoad() {
         super.viewDidLoad()
